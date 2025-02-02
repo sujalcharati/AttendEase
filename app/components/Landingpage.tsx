3c4274670dc8a9fb1c589ad3c9d72163ac33a9ee
sujalcharati <sujalc024@gmail.com>
2025-02-02 22:01:27 +0530

    hero layout

---
+++ app/components/Landingpage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Landingpage = () => {
+    return (
+        <div>
+            <div className="flex flex-col justify-start items-start h-96 font-semibold text-5xl text-left text-gray-900 pl-28 mt-48">
+                <div>Effortlessly</div>
+                <div>track</div>
+                <div>attendance</div>
+            </div>
+            <div>
+            Say goodbye to boring forms. Meet Tally — the free, intuitive form builder you’ve been looking for.
+            </div>
+        </div>
+    );
+};
+
+export default Landingpage
