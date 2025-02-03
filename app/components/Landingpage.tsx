7b61a292679e79709b6623b454ed891cc732e426
sujalcharati <sujalc024@gmail.com>
2025-02-03 22:52:07 +0530

    hero section layout

--- app/components/Landingpage.tsx
+++ app/components/Landingpage.tsx
@@ -3,13 +3,15 @@ import React from 'react'
 const Landingpage = () => {
     return (
         <div>
-            <div className="flex flex-col justify-start items-start h-96 font-semibold text-5xl text-left text-gray-900 pl-28 mt-48">
-                <div>Effortlessly</div>
-                <div>track</div>
-                <div>attendance</div>
+            <div className="flex flex-col justify-start items-start  font-semibold text-5xl text-left text-gray-900 pl-28 mt-48">
+                <div className="mb-4">Effortlessly</div>
+                <div className="mb-4">track</div>
+                <div className="mb-4">attendance</div>
             </div>
-            <div>
-            Say goodbye to boring forms. Meet Tally — the free, intuitive form builder you’ve been looking for.
+            <div className='text-black flex flex-col pl-28 mt-4 text-2xl'>
+                <div>Say goodbye to boring forms.Meet Tally — intuitive </div>
+                <div> the free,you’ve been looking for.</div>
+                   
             </div>
         </div>
     );
