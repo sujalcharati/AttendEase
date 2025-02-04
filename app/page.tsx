101acb7e9659c46f129a06f2a3b818fe92e3eb43
sujalcharati <sujalc024@gmail.com>
2025-02-04 16:20:00 +0530

    next-auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+import { signIn, signOut } from "next-auth/react";
 import Landingpage from "./components/Landingpage";
 
 export default function Home() {
@@ -5,7 +7,12 @@ export default function Home() {
     <div className="bg-white min-h-screen">
       <nav className="flex justify-between items-center p-4 bg-gray-100">
         <div className="text-blue-600 text-4xl font-serif pl-28">Attendease</div>
-        <button className="bg-blue-500 text-white px-4 py-2 rounded">Login</button>
+
+        <div className="flex space-x-4">
+          <button onClick={ ()=> signIn()} className="text-black py-2">Log in</button>
+          <button  className="bg-blue-500 text-white px-4 py-2 rounded">Get started</button>
+        </div>
+        {/* <button className="bg-blue-500 text-white px-4 py-2 rounded">Get started</button> */}
       </nav>
       <Landingpage/>
     </div>
