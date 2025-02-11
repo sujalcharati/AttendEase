d0adbe081f173790216bab73239abee322b8b3ac
sujalcharati <sujalc024@gmail.com>
2025-02-11 23:09:55 +0530

    signin endpoint

---
+++ app/api/auth/signin/route.ts
@@ -0,0 +1,59 @@
+import { connectDB } from '@/lib/db';
+import User from '@/models/user';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { NextResponse } from 'next/server';
+connectDB();
+
+export async function POST(req:Request) {
+    try{
+
+        const {email, password} = await req.json();
+        const user = await User.findOne({email})
+
+        if(!user){
+            return Response.json({error: 'user does not exists'})
+        }
+
+        console.log('user exists')
+
+        if (!user.password) {
+            return Response.json({ error: 'Invalid credentials' });
+        }
+        const isMatch = await bcrypt.compare(password, user.password as string);
+
+
+        if (!isMatch) {
+            return Response.json({ error: 'Invalid credentials' });
+        }
+
+
+        const tokenData = {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+        if (!process.env.TOKEN_SECRET) {
+            throw new Error('TOKEN_SECRET is not defined');
+        }
+        const token =  jwt.sign(tokenData, process.env.TOKEN_SECRET, { expiresIn: '1d' });
+
+        const response = NextResponse.json({
+            message: 'logged in successfully',
+            success: 'true'
+        });
+
+
+        response.cookies.set("token",token,{
+            httpOnly:true
+        })
+        
+        return response
+
+    
+
+
+    } catch (error: any) {
+        return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+    }
+}
