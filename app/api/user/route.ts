ca4de8b49e12ae7d0a323a2b4697ca11a8ac6c56
sujalcharati <sujalc024@gmail.com>
2025-07-02 22:14:45 +0530

    user router

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -2,7 +2,7 @@ import { getServerSession } from "next-auth"
 import { NextResponse } from "next/server";
 import { authOptions } from "@/lib/authOptions";
 
-export async function GET(request: Request) {
+export async function GET() {
     try {
         const session = await getServerSession(authOptions);
         
