406a9f709a12b00cd0415fafea3f808d3124b200
sujalcharati <sujalc024@gmail.com>
2025-07-02 22:13:01 +0530

    user router

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,13 +1,25 @@
-// import { NEXT_AUTH_CONFIG } from "@/lib/auth";
-// import { getServerSession } from "next-auth"
-// import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth"
+import { NextResponse } from "next/server";
+import { authOptions } from "@/lib/authOptions";
 
-// export async function GET() {
-
-
-//     const session = await getServerSession(NEXT_AUTH_CONFIG);
-//     console.log(session);
-//     return NextResponse.json({
-//         name: session?.user?.name
-//     })
-// }
+export async function GET(request: Request) {
+    try {
+        const session = await getServerSession(authOptions);
+        
+        if (!session?.user) {
+            return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
+        }
+        
+        return NextResponse.json({
+            name: session?.user?.name,
+            email: session?.user?.email,
+            id: (session.user as { id: string }).id
+        })
+    } catch (error) {
+        console.error("Error fetching user:", error)
+        return NextResponse.json(
+            { error: "Failed to fetch user data" },
+            { status: 500 }
+        )
+    }
+}
