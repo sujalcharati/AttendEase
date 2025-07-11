9cebe3e434334462c917d25647ac18b6b35acb65
sujalcharati <sujalc024@gmail.com>
2025-07-11 16:27:49 +0530

    uncommented message

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -1,36 +1,36 @@
-"use client"
+// "use client"
 
-import * as React from "react"
-import { ChatList } from "@/components/chat/chat-list"
-import { ChatWindow } from "@/components/chat/chat-window"
-import { CreateGroupDialog } from "@/components/chat/create-group"
+// import * as React from "react"
+// import { ChatList } from "@/components/chat/chat-list"
+// import { ChatWindow } from "@/components/chat/chat-window"
+// import { CreateGroupDialog } from "@/components/chat/create-group"
 
 
 
-export default function SocialPage() {
-  const [selectedChat, setSelectedChat] = React.useState<string>()
-  interface ChatWindowProps {
-    chatId: string
-  }
+// export default function SocialPage() {
+//   const [selectedChat, setSelectedChat] = React.useState<string>()
+//   interface ChatWindowProps {
+//     chatId: string
+//   }
 
-  return (
-    <div className="grid h-[calc(100vh-4rem)] grid-cols-[350px_1fr]">
-      <div className="border-r">
-        <div className="p-4">
-          <CreateGroupDialog />
-        </div>
-        <ChatList selectedChat={selectedChat} onSelectChat={setSelectedChat} />
-      </div>
-      <div className="flex flex-col">
-        {selectedChat ? (
-          <ChatWindow chatId={selectedChat} />
-        ) : (
-          <div className="flex flex-1 items-center justify-center">
-            <p className="text-muted-foreground">Select a conversation to start chatting</p>
-          </div>
-        )}
-      </div>
-    </div>
-  )
-}
+//   return (
+//     <div className="grid h-[calc(100vh-4rem)] grid-cols-[350px_1fr]">
+//       <div className="border-r">
+//         <div className="p-4">
+//           <CreateGroupDialog />
+//         </div>
+//         <ChatList selectedChat={selectedChat} onSelectChat={setSelectedChat} />
+//       </div>
+//       <div className="flex flex-col">
+//         {selectedChat ? (
+//           <ChatWindow chatId={selectedChat} />
+//         ) : (
+//           <div className="flex flex-1 items-center justify-center">
+//             <p className="text-muted-foreground">Select a conversation to start chatting</p>
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   )
+// }
 
