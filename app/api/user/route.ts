0ea1aec0d42e2833ebdba246553fef0c2247adf9
sujalcharati <sujalc024@gmail.com>
2025-07-11 16:30:52 +0530

    commented api/user

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,25 +1,25 @@
-import { getServerSession } from "next-auth"
-import { NextResponse } from "next/server";
-import { authOptions } from "@/lib/authOptions";
+// import { getServerSession } from "next-auth"
+// import { NextResponse } from "next/server";
+// import { authOptions } from "@/lib/authOptions";
 
-export async function GET(request: Request) {
-    try {
-        const session = await getServerSession(authOptions);
+// export async function GET(request: Request) {
+//     try {
+//         const session = await getServerSession(authOptions);
         
-        if (!session?.user) {
-            return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
-        }
+//         if (!session?.user) {
+//             return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
+//         }
         
-        return NextResponse.json({
-            name: session?.user?.name,
-            email: session?.user?.email,
-            id: (session.user as { id: string }).id
-        })
-    } catch (error) {
-        console.error("Error fetching user:", error)
-        return NextResponse.json(
-            { error: "Failed to fetch user data" },
-            { status: 500 }
-        )
-    }
-}
+//         return NextResponse.json({
+//             name: session?.user?.name,
+//             email: session?.user?.email,
+//             id: (session.user as { id: string }).id
+//         })
+//     } catch (error) {
+//         console.error("Error fetching user:", error)
+//         return NextResponse.json(
+//             { error: "Failed to fetch user data" },
+//             { status: 500 }
+//         )
+//     }
+// }
